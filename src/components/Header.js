8aa45ccb284c925d358e9b6433ec549d1eed6bdc
cm <etchiedaniel1@gmail.com>
2022-10-17 17:28:10 +0100

    added logo and avatar in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import classes from "./Header.module.css"
 import retink_avater from "./images/Retink-avatar.png"
+import logo from "./images/Logo.png"
 
 const Header = () => {
     return (
         <header className={classes.header}>
             <section>
-
+                <img src={logo} alt="retink_logo" />
             </section>
             <img className={classes.avater_img} src={retink_avater} alt="retink-bot" />
         </header>
